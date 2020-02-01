aa240b09b0a97e341e5dc577cb1c6ee26981bf91
Simon Jordan <simon@thepolygonloft.com>
2020-02-01 17:11:29 +0000

    Beam blast effect

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,6 +6,8 @@ public class PlayerControls : MonoBehaviour
 {
     public GameObject pivot;
 
+    public GameObject beam;
+
     public float speed;
 
     enum DIRECTION { LEFT, RIGHT };
@@ -153,11 +155,33 @@ public class PlayerControls : MonoBehaviour
         return ret;
     }
 
+
+    Color basicColour = Color.white;
+    Color targetColour = new Vector4(1f, 1f, 1f, 0f);
+
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        beam.GetComponent<SpriteRenderer>().color = basicColour;
+        beam.GetComponent<SpriteRenderer>().material.color = basicColour;
+
+        float alpha = beam.GetComponent<SpriteRenderer>().material.color.a;
+
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha, aValue, t));
+            beam.GetComponent<SpriteRenderer>().material.color = newColor;
+
+            yield return null;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            StartCoroutine(FadeTo(0.0f, 0.25f));
+
             if (!didDetect)
             {
                 Debug.Log("MISS!");
