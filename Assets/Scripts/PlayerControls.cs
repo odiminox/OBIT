dff82ad8fbed1b7dc54b3dde5f5efcf2a2e4a9e2
Simon Jordan <simon@thepolygonloft.com>
2020-02-01 12:59:30 +0000

    Player node detection

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -17,6 +17,48 @@ public class PlayerControls : MonoBehaviour
         
     }
 
+    public void NodeDetection()
+    {
+        RaycastHit2D hitInner = Physics2D.Raycast(transform.position, Vector2.up);
+
+        if (hitInner.collider != null)
+        {
+            Node node = hitInner.transform.GetComponent<Node>();
+
+            if (node != null)
+            {
+                if (node.nodePosition == Node.NODEPOSITION.INNER)
+                {
+                    Debug.Log("INNER");
+                }
+            }
+        }
+
+        RaycastHit2D hitOuter = Physics2D.Raycast(transform.position, -Vector2.up);
+
+        if (hitOuter.collider != null)
+        {
+            if (hitOuter.transform.name != "Player")
+            {
+                Node node = hitOuter.transform.GetComponent<Node>();
+
+                if (node != null)
+                {
+                    if (node.nodePosition == Node.NODEPOSITION.OUTER)
+                    {
+                        Debug.Log("OUTER");
+                    }
+                }
+
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        NodeDetection();
+    }
+
     // Update is called once per frame
     void Update()
     {
