ad1d9dd2b9f276357fdea0f29f2ae835bfb9692c
Simon Jordan <simon@thepolygonloft.com>
2020-02-01 12:44:52 +0000

    Inner and outer nodes

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public enum NODEPOSITION { INNER, OUTER };
+    public NODEPOSITION nodePosition;
+
+    public float innerDepth = -0.038f;
+    public float outerDepth = -0.0824f;
+
+    public float angle;
+
+    private GameObject pivot;
+
+    private Vector3 rotationAxis;
+
+    Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (nodePosition)
+        {
+            case NODEPOSITION.INNER:
+                {
+                    transform.localPosition = new Vector2(transform.position.x, innerDepth);
+
+                    break;
+                }
+            case NODEPOSITION.OUTER:
+                {
+                    transform.localPosition = new Vector2(transform.position.x, outerDepth);
+
+                    break;
+                }
+            default:
+                break;
+
+        }
+
+        rotationAxis = new Vector3(0f, 0f, 1f);
+
+        pivot = GameObject.Find("Pivot");
+
+        direction = transform.position - pivot.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rot = Quaternion.AngleAxis(angle, rotationAxis);
+        transform.position = pivot.transform.position + rot * direction;
+        transform.position = new Vector3(transform.position.x, transform.position.y, 1f);
+        transform.localRotation = rot;
+    }
+}
